671692782a56548c4ab2412bcf35ce7c67f13ef1
“popcor255” <popcor255@gmail.com>
2019-11-16 22:28:22 -0500

    changes

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,15 +1,51 @@
 import 'dart:async';
+import 'dart:core' as prefix0;
+import 'dart:core';
 import 'package:bloc/bloc.dart';
+import 'package:tdd_development/core/util/input_converter.dart';
+import 'package:tdd_development/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:tdd_development/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:meta/meta.dart';
 import './bloc.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE = 'Invalid Input';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    @required this.getConcreteNumberTrivia,
+    @required this.getRandomNumberTrivia,
+    @required this.inputConverter,
+  }) {
+    assert(getConcreteNumberTrivia != null);
+    assert(getRandomNumberTrivia != null);
+    assert(inputConverter != null);
+  }
+
   @override
-  NumberTriviaState get initialState => InitialNumberTriviaState();
+  NumberTriviaState get initialState => InitialNumberTriviaState;
 
   @override
   Stream<NumberTriviaState> mapEventToState(
     NumberTriviaEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is GetTriviaForConcreteNumber) {
+      final input = inputConverter.stringToUnsignedInteger(event.numberString);
+
+      yield* input.fold(
+        (failure) async* {
+          yield Error(
+            message: INVALID_INPUT_FAILURE_MESSAGE,
+          );
+        },
+        (integer) => throw prefix0.UnimplementedError(),
+      );
+      yield Error(message: 'Invalid Input');
+    }
   }
 }
--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,10 +1,31 @@
 import 'package:equatable/equatable.dart';
+import 'package:tdd_development/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:meta/meta.dart';
 
 abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
 }
 
-class InitialNumberTriviaState extends NumberTriviaState {
+NumberTriviaState get InitialNumberTriviaState => Empty();
+
+class Empty extends NumberTriviaState {
   @override
+  // TODO: implement props
   List<Object> get props => [];
+}
+
+class Loading extends NumberTriviaState {
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [message];
 }
